fcb6da26a533efa4e2a5fb2b97363d9ca6a3d2fc
ainscroteau <62759084+ainscroteau@users.noreply.github.com>
2020-03-27 12:07:45 -0400

    Entire Solution

---
+++ TeddyAttack/TeddyAttack/CollisionSide.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeddyAttack
+{
+    /// <summary>
+    /// An enumeration for possible collision sides
+    /// </summary>
+    public enum CollisionSide
+    {
+        None,
+        Bottom,
+        Left,
+        Right,
+        Top
+    }
+}
